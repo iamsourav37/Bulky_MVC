b4d79ddf629cf5196405ae2c956bfaaf2783979d
iamsourav37 <sourav.g29@outlook.com>
2024-03-21 20:39:20 +0530

    bootswatch theme is added, category listed page is designed, database is also connected

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ApplicationDBContext _db;
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            this._db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var categoryDetails = _db.Category.ToList();
+            return View(categoryDetails);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
--- BulkyWeb/Data/ApplicationDBContext.cs
+++ BulkyWeb/Data/ApplicationDBContext.cs
@@ -7,9 +7,23 @@ namespace BulkyWeb.Data
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         {
-            
+
         }
 
-        DbSet<Category> Category { get; set; }
+        public DbSet<Category> Category { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            List<Category> categories = new List<Category>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                categories.Add(new Category() { Id = i + 1, DisplayOrder = i + 1, Name = $"Category {i + 1}" });
+            }
+
+            modelBuilder.Entity<Category>().HasData(categories);
+        }
     }
 }
