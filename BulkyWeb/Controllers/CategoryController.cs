608062423f5ebcfde11b55446d6233fb3d8c258b
iamsourav37 <sourav.g29@outlook.com>
2024-03-22 21:15:47 +0530

    Edit, Delete method is implemented

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -43,5 +43,69 @@ namespace BulkyWeb.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? categoryId)
+        {
+            if (categoryId == null)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Category.FirstOrDefault(x => x.Id == categoryId);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? categoryId)
+        {
+            if (categoryId is null)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Category.FirstOrDefault(cate => cate.Id == categoryId);
+            if (categoryFromDb is null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteCategory(int? categoryId)
+        {
+            if (categoryId is null)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Category.FirstOrDefault(cate => cate.Id == categoryId);
+            if (categoryFromDb is null)
+            {
+                return NotFound();
+            }
+
+            //Delete the Category
+            _db.Category.Remove(categoryFromDb);
+            _db.SaveChanges();
+            Console.WriteLine($"Delete Successfull. Category Details: {categoryFromDb.Id}, {categoryFromDb.Name}, {categoryFromDb.DisplayOrder}");
+            return RedirectToAction("Index");
+        }
     }
 }
