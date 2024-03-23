63f8e7e874732cc80b722dec376b9a66abc4aaab
iamsourav37 <sourav.g29@outlook.com>
2024-03-23 15:08:10 +0530

    Area is added, public and Admin areas are created

---
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDBContext _dBContext;
+
+        public CategoryRepository(ApplicationDBContext dBContext) : base(dBContext)
+        {
+            this._dBContext = dBContext;
+        }
+        public void Save()
+        {
+            _dBContext.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _dBContext.Category.Update(category);
+        }
+    }
+}
---
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Bulky.DataAccess.Repository
+{
+    public class Repository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDBContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(ApplicationDBContext dBContext)
+        {
+            this._dbContext = dBContext;
+            this._dbSet = this._dbContext.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public T Get(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+        {
+            IQueryable<T> query = _dbSet;
+            query = query.Where(predicate);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = _dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+    }
+}
